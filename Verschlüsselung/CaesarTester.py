93012ecb91f240c87f4d876a1a878715e44dc7a5
viktor.tevosyan <viktor.tevosyan@stud.uni-goettingen.de>
2023-08-10 14:31:29 +0200

    Verschlüsselung

---
+++ Verschlüsselung/CaesarTester.py
@@ -0,0 +1,14 @@
+alphabet ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def caesar_unlocker(message):
+    str = ""
+    for i in range(26):
+        print(caesar_decrypt(message, i))
+
+def caesar_decrypt(message, key):
+    str = ""
+    for c in message:
+        str += alphabet[(alphabet.index(c) - key) % 26]
+    return str
+
+
